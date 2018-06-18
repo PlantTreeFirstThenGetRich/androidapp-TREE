771efb4151635c714b12e3179d2c57bccb928418
beck <503646976@qq.com>
2018-06-18 12:04:43 +0800

    Add background music and messagebox, adjust UI. [Shop]

---
+++ Assets/Actor/PacketClick.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PacketClick : MonoBehaviour {
+    public bool packetopenornot;
+    public GameObject packetcanvas;
+    // Use this for initialization
+
+    private void Awake()
+    {
+        packetcanvas = GameObject.Find("GridPanel");
+        packetopenornot = false;
+    }
+    void Start () {
+        GameObject imageobj = GameObject.Find("Packet");
+        packetcanvas.gameObject.SetActive(false);
+        Button packet = imageobj.GetComponent<Button>();
+        packet.onClick.AddListener(delegate()
+        {
+            this.PacketButtonClicked();
+        });
+    }
+	
+    void PacketButtonClicked()
+    {
+        if (packetopenornot == false)
+        {
+            packetopenornot = true;
+        }
+        else if (packetopenornot)
+        {
+            packetopenornot = false;
+        }
+        Debug.Log(packetopenornot);
+        CanvasManager.Instance.InitPacket(packetopenornot, packetcanvas);
+    }
+}
---
+++ Assets/Actor/TreeGrowth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeGrowth : MonoBehaviour {
+	public Sprite[] sprites;
+	SpriteRenderer spriteRenderer;
+
+	void Start () {
+		int growth_second = SuperGameMaster.saveData.treeGrowthTimeSec;
+		spriteRenderer = GetComponent<Renderer>() as SpriteRenderer;
+        Debug.Log("TreeGrowth.cs start" + sprites.Length);
+        int growth_days = (int)(growth_second / 60 / 60 / 24);
+		// test minute
+		//int growth_days = (int)(growth_second / 60);
+		Debug.Log ("[TreeGrowth] in start: the growth_days is: " + growth_days);
+		if (growth_days <= 1) {
+			Debug.Log ("[TreeGrowth] in start: 0 tree");
+            Debug.Log("TreeGrowth.cs " + sprites.Length);
+            spriteRenderer.sprite = sprites [0];
+		} else if (3 >= growth_days && growth_days > 1) {
+			Debug.Log ("[TreeGrowth] in start: 1 tree");
+            Debug.Log("TreeGrowth.cs " + sprites.Length);
+            spriteRenderer.sprite = sprites [1];
+		} else if (growth_days > 3) {
+			Debug.Log ("[TreeGrowth] in start: 2 tree");
+            Debug.Log("TreeGrowth.cs " + sprites.Length);
+            spriteRenderer.sprite = sprites [2];
+		}
+	}
+}
